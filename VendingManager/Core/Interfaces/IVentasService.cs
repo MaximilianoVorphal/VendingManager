8cc7ccf4f21ae3b16f664a2a028897ccbc6aa07f
MaximilianoVorphal <maximilianovorphal@gmail.com>
2025-12-21 02:01:16 -0300

    feat: add sales reporting page with filtering, key performance indicators, detailed transactions, and Excel export.

--- VendingManager/Core/Interfaces/IVentasService.cs
+++ VendingManager/Core/Interfaces/IVentasService.cs
@@ -6,9 +6,9 @@ namespace VendingManager.Core.Interfaces
     {
         Task<List<MaquinaSimpleDto>> GetMaquinasAsync();
         Task<DashboardStats> GetDashboardStatsAsync(int maquinaId);
-        Task<ReporteDto> GetReporteRangoAsync(DateTime inicio, DateTime fin, int maquinaId);
+        Task<ReporteDto> GetReporteRangoAsync(DateTime inicio, DateTime fin, int maquinaId, bool includePhantom = false);
         Task<InformeFinancieroDto> GetInformeFinancieroAsync(DateTime inicio, DateTime fin, int maquinaId);
-        Task<(byte[] content, string fileName)> ExportarReporteAsync(DateTime inicio, DateTime fin, int maquinaId);
+        Task<(byte[] content, string fileName)> ExportarReporteAsync(DateTime inicio, DateTime fin, int maquinaId, bool includePhantom = false);
         Task FixDatesAsync();
         Task ImportarVentasMaquinaAsync(Stream stream, string fileName);
         Task ImportarTransbankAsync(Stream stream, string fileName);
